b7b70b5dfd8bcaaaad8361050c6dad3222d82e10
Ricky Arora <your_email_here>
2020-10-30 18:15:10 -0400

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Ricky Arora on 2020-10-30.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
