dc70f1ecddf82a81cc1b5d33979861214182cee4
Ricky Arora <your_email_here>
2020-11-09 16:52:06 -0500

    done sliders

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,30 +9,70 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var alertIsVisible: Bool = false    
+    @State var alertIsVisible = false
+    @State var sliderValue = 50.0
+    @State var target = Int.random(in: 1...100)
     
     var body: some View {
-        VStack {
-            Text("Welcome to my first app")
-                .fontWeight(.semibold)
-                .foregroundColor(Color.green)
-                .padding()
-            Button(action:  {
-                print("Button pressed!")
-                self.alertIsVisible = true
-            }) {
-                Text("Hit me")
-            }
-            .alert(isPresented: $alertIsVisible) {
-                () -> Alert in
-                return Alert(title: Text("Hello there!"), message: Text("This is my first popup"), dismissButton: .default(Text("Code")))
+        HStack {
+            VStack {
+                HStack {
+                    Text("Put the bullseye as you can to:")
+                    Text("\(target)")
+                }
+                Spacer()
+                // Slider row
+                HStack {
+                    Text("1")
+                    Slider(value: $sliderValue, in: 1...100)
+                    Text("100")
+                }
+                Spacer()
+                // Button row
+                Button(action:  {
+                    print("Button pressed!")
+                    alertIsVisible = true
+                }) {
+                    Text("Hit me")
+                    
+                }
+                .alert(isPresented: $alertIsVisible) {
+                    () -> Alert in
+                    return Alert(title: Text("Hello there!"), message: Text("The slider's value is: \(Int(self.sliderValue.rounded())). \n " +
+                                                                                "You scored \(self.pointsForCurrentRound()) points this round"), dismissButton: .default(Text("Code")))
+                }
+                Spacer()
+                HStack {
+                        Button(action:  {
+                            print("Button pressed!")
+                        }) {
+                            Text("Start Over")}
+                        Spacer()
+                        Text("Score:")
+                        Text("ggggg")
+                        Spacer()
+                        Text("Round")
+                        Text("999")
+                        Spacer()
+                        Button(action:  {
+                            print("Button pressed!")
+                        }) {
+                            Text("Info")}
+                    }
+                .padding(.bottom, 20)
+                }
             }
         }
+    func pointsForCurrentRound() -> Int {
+        let roundedValue = Int(sliderValue.rounded())
+        let difference = abs(roundedValue - target)
+        let awardedPoints = 100 - difference
+        return awardedPoints
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewLayout(.fixed(width: 896, height: 414))
     }
 }
