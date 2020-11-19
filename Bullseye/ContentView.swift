eb96da2287e16648553775f9c16d14a528b72eba
Ricky Arora <your_email_here>
2020-11-19 09:44:42 -0500

    trying to figure out some errors

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     @State var alertIsVisible = false
     @State var sliderValue = 50.0
     @State var target = Int.random(in: 1...100)
+    @State var total = 0
     
     var body: some View {
         HStack {
@@ -38,7 +39,7 @@ struct ContentView: View {
                 }
                 .alert(isPresented: $alertIsVisible) {
                     () -> Alert in
-                    return Alert(title: Text("Hello there!"), message: Text("The slider's value is: \(Int(self.sliderValue.rounded())). \n " +
+                    return Alert(title: Text("Hello there!"), message: Text("The slider's value is: \(Int(slideValueRounded())). \n " +
                                                                                 "You scored \(self.pointsForCurrentRound()) points this round"), dismissButton: .default(Text("Code")))
                 }
                 Spacer()
@@ -49,7 +50,7 @@ struct ContentView: View {
                             Text("Start Over")}
                         Spacer()
                         Text("Score:")
-                        Text("ggggg")
+                        Text("\(total)")
                         Spacer()
                         Text("Round")
                         Text("999")
@@ -63,11 +64,18 @@ struct ContentView: View {
                 }
             }
         }
+    func slideValueRounded() -> Int {
+        Int(sliderValue.rounded())
+    }
+    
     func pointsForCurrentRound() -> Int {
-        let roundedValue = Int(sliderValue.rounded())
-        let difference = abs(roundedValue - target)
-        let awardedPoints = 100 - difference
-        return awardedPoints
+        let points = 100 - abs(target - slideValueRounded())
+        addToTotalPoints(pointsToAdd: points)
+        return points
+    }
+    
+    func addToTotalPoints(pointsToAdd: Int) {
+        total = pointsToAdd + total
     }
 }
 
